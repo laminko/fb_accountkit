d7669729f8904647443e92909065976da7d35828
La Min Ko <lminko.lmk@gmail.com>
2019-07-22 23:46:51 +0630

    added remaining methods.

--- fb_accountkit.py
+++ fb_accountkit.py
@@ -1,4 +1,3 @@
-import http
 import hmac
 import hashlib
 import requests
@@ -13,10 +12,14 @@ class FBAccountKit(object):
 
     __apis = {
         'me': 'https://graph.accountkit.com/{version}/me',
-        'access_token': 'https://graph.accountkit.com/{version}/access_token'
+        'access_token': 'https://graph.accountkit.com/{version}/access_token',
+        'logout': 'https://graph.accountkit.com/{version}/logout',
+        'invalidate_all_tokens': ' https://graph.accountkit.com/{version}/{{account_id}}/invalidate_all_tokens',
+        'account_removal': 'https://graph.accountkit.com/{version}/{{account_id}}',
+        'accounts': 'https://graph.accountkit.com/{version}/{app_id}/accounts/'
     }
 
-    def __init__(self, app_id, app_secret, version='v1.1', debug=False):
+    def __init__(self, app_id, app_secret, version='v1.1', debug=False, logger=None):
         self.App_ID = app_id
         self.App_secret = app_secret
         self.API_Version = version
@@ -26,42 +29,50 @@ class FBAccountKit(object):
         )
         self.__user_access_token = None
         self.__debug = debug
+        self.__logger = logger
         self.__prepare_api_urls()
 
     def __prepare_api_urls(self):
-        for k, v in self.__apis.items():
-            self.__apis[k] = v.format(version=self.API_Version)
+        """
+        Prepare API urls.
 
-    def console_print(self, *args, **kwargs):
+        :return:
+        """
+        for k, v in self.__apis.items():
+            self.__apis[k] = v.format(version=self.API_Version, app_id=self.App_ID)
+
+    def __console_print(self, *args, **kwargs):
         """
         Console print for debugging.
+        logger will be used if it is provided, else print() will be used instead.
 
         :param args:
         :param kwargs:
         :return:
         """
         if self.__debug:
-            print(*args, **kwargs)
+            writer = print
+            if self.__logger:
+                writer = self.__logger.debug
+            writer(*args, **kwargs)
 
     def retrieve_user_access_token(self, auth_code):
         """
         Retrieve User Access Token using Authorization Code provided by Client.
 
+        REF: https://developers.facebook.com/docs/accountkit/graphapi#retrieving-user-access-tokens-with-an-authorization-code
+
         :param auth_code:
         :return:
         """
-        data = dict()
         params = dict(
             grant_type='authorization_code',
             code=auth_code,
             access_token=self.App_Access_Token
         )
         url = self.__apis.get('access_token')
-        resp = requests.get(url, params=params)
-        self.console_print(resp.text)
-        if resp.status_code == http.HTTPStatus.OK:
-            data = resp.json()
-            self.__user_access_token = data.get('access_token')
+        data = self.call_requests('get', url, params=params)
+        self.__user_access_token = data.get('access_token')
         return data
 
     def get_app_secret_proof(self, access_token=None):
@@ -84,17 +95,110 @@ class FBAccountKit(object):
         """
         Get user session using User Access Token
 
+        REF: https://developers.facebook.com/docs/accountkit/graphapi#at-validation
+
         :return:
         """
 
-        data = dict()
         params = dict(
             access_token=self.__user_access_token,
             appsecret_proof=self.get_app_secret_proof()
         )
         url = self.__apis.get('me')
-        resp = requests.get(url, params=params)
-        self.console_print(resp.text)
-        if resp.status_code == http.HTTPStatus.OK:
-            data = resp.json()
-        return data
+        return self.call_requests('get', url, params=params)
+
+    def get_accounts_after_removal(self, item_per_page=100):
+        """
+        WARNING: USE WITH CAUTION.
+
+        Retrieve user data after removal.
+
+        REF: https://developers.facebook.com/docs/accountkit/graphapi#deleteapp
+
+        :param item_per_page:
+        :return:
+        """
+
+        params = dict(
+            access_token=self.App_Access_Token,
+            limit=item_per_page
+        )
+        url = self.__apis.get('accounts')
+        return self.call_requests('get', url, params=params)
+
+    def logout(self):
+        """
+        WARNING: USE WITH CAUTION.
+
+        Logout session of the user.
+
+        REF: https://developers.facebook.com/docs/accountkit/graphapi#logout
+
+        :return:
+        """
+        params = dict(
+            access_token=self.__user_access_token,
+            appsecret_proof=self.get_app_secret_proof(self.__user_access_token)
+        )
+        url = self.__apis.get('logout')
+        return self.call_requests('post', url, params=params)
+
+    def logout_all_session(self, account_id):
+        """
+        WARNING: USE WITH CAUTION.
+
+        Logout all sessions of given account_id.
+
+        REF: https://developers.facebook.com/docs/accountkit/graphapi#logout
+
+        :param account_id:
+        :return:
+        """
+        url = self.__apis.get('invalidate_all_tokens').format(account_id=account_id)
+        params = dict(
+            access_token=self.App_Access_Token
+        )
+        return self.call_requests('post', url, params=params)
+
+    def remove(self, account_id):
+        """
+        WARNING: USE WITH CAUTION.
+
+        Delete an account from the database stored on Account Kit servers.
+
+        :param account_id:
+        :return:
+        """
+        url = self.__apis.get('account_removal').format(account_id=account_id)
+        params = dict(
+            access_token=self.App_Access_Token
+        )
+        return self.call_requests('delete', url, params=params)
+
+    def call_requests(self, method, url, *args, **kwargs):
+        """
+        A wrapper function for python-requests.
+        Raise if requests.exceptions.HTTPError occurred.
+
+        :param method:
+        :param url:
+        :param args:
+        :param kwargs:
+        :return:
+        """
+        format_http_request = '{http_method} {status_code} {url}'
+        format_func_params = '{arg_type} {args}'
+        if args:
+            self.__console_print(format_func_params.format(arg_type='ARGS', args=args))
+        if kwargs:
+            self.__console_print(format_func_params.format(arg_type='KWARGS', args=kwargs))
+        http_method = getattr(requests, method)
+        resp = http_method(url, *args, **kwargs)
+        self.__console_print(
+            format_http_request.format(
+                http_method=method.upper(),
+                url=url,
+                status_code=resp.status_code))
+        self.__console_print(resp.text)
+        resp.raise_for_status()
+        return resp.json()
